c903b9677a8d3e1a191aa39869e6c23311fe7c9b
Yuki Kodama <endflow.net@gmail.com>
2020-02-11 14:26:27 +0900

    Fix example

--- examples/simple.rs
+++ examples/simple.rs
@@ -1,13 +1,13 @@
 extern crate actix_delay;
 
+use actix_delay::middleware::Delay;
 use actix_web::{web::resource, App, HttpServer};
-use std::io::Result;
 
 #[actix_rt::main]
-async fn main() -> Result<()> {
+async fn main() -> std::io::Result<()> {
     HttpServer::new(|| {
         App::new()
-            .wrap(actix_delay::middleware::Delay::new(5000))
+            .wrap(Delay::new(5000))
             .service(resource("/").to(|| async { "Delay" }))
     })
     .bind("127.0.0.1:8080")?
